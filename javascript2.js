ce81a415ac51d9c6f2f0d5a615baa3e050721129
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-12-06 20:43:24 -0500

    Add files via upload

---
+++ javascript2.js
@@ -0,0 +1,10 @@
+let colorData = [];
+let htmlFeed = "";
+const modeBtn = document.querySelector("#mode-btn");
+const mode = document.getElementById("mode");
+console.log(mode);
+console.log(modeBtn);
+
+modeBtn.addEventListener("click", function(){
+    console.log("hello");
+})
