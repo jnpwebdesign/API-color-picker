5a2849eced0c53d516504d5a066b24ebdb53e767
Julie Napear <72626295+jnpwebdesign@users.noreply.github.com>
2022-12-09 18:24:54 -0500

    12/09/22 18:24

--- javascript.js
+++ javascript.js
@@ -4,13 +4,24 @@ const modeBtn = document.querySelector("#mode-btn");
 const mode = document.getElementById("mode");
 const form = document.getElementById("get-color-scheme-form");
 const color = document.getElementById("color");
+let originalColor = "";
 
-
-//hex color displays 
+//original hex color displays 
 color.addEventListener("change", function(e){
     e.preventDefault();
-    document.getElementById("hex-value").textContent = `Hex value: ${color.value}`
-
+    document.getElementById("hex-value").innerHTML = `
+        Original Color Hex value: ${color.value}
+        <button id="copy-OG-btn" class="copy-btn">
+            <i class="fa-regular fa-copy"></i>
+        </button>
+        `
+    document.getElementById("step2-container").classList.remove("hidden");    
+    modeBtn.classList.remove("hidden");   
+    originalColor = color.value;
+    const copyBtn = document.getElementById("copy-OG-btn");    
+    copyBtn.addEventListener("click", function(e){
+        navigator.clipboard.writeText(originalColor);
+    });
 });
 
 
@@ -36,10 +47,27 @@ renderColors = colorData => {
                 </div>
                 <div class="color-name" id="color${i}-name">
                     Hex value: ${colorData[i].hex.value}
+                    <button id="copy-btn${i}" class="copy-btn" data-color${i}="color${i}-name" >
+                        <i class="fa-regular fa-copy" data-color${i}="color${i}-name"></i>
+                    </button>
                 </div>
             </div>    
             `
     }
     document.getElementById("main-container").innerHTML = htmlFeed;
+    document.addEventListener("click", function(e){
+        if(e.target.dataset.color0) {
+            navigator.clipboard.writeText(colorData[0].hex.value);
+        } else if(e.target.dataset.color1) {
+            navigator.clipboard.writeText(colorData[1].hex.value);
+        } else if(e.target.dataset.color2) {
+            navigator.clipboard.writeText(colorData[2].hex.value);
+        } else if(e.target.dataset.color3) {
+            navigator.clipboard.writeText(colorData[3].hex.value);
+        } else if(e.target.dataset.color4) {
+            navigator.clipboard.writeText(colorData[4].hex.value);
+        }
+    })
 }
+
 
